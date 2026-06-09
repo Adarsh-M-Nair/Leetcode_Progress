ed8737f0bccf5add379026ee63e44de931717ccc
Adarsh M. Nair <adarshvaikhary@gmail.com>
2026-06-09 14:22:21 +0530

    Valid Palindrome

---
+++ Easy/125. Valid Palindrome.py
@@ -0,0 +1,11 @@
+def isPalindrome(self, s: str) -> bool:
+        n=""
+        for i in s:
+            if i>='a' and i<='z' or i>='A' and i<='Z' or i>='0' and i<='9':
+                n+=i
+        s=n.lower()
+        rev=s[::-1]
+        if rev==s:
+            return True
+        else:
+            return False
