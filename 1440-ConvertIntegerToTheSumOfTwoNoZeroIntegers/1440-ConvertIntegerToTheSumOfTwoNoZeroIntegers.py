f32286cf7c493dca107f9cee26ed7216e9c0606f
Adarsh M. Nair <106982376+Adarsh-M-Nair@users.noreply.github.com>
2026-07-18 20:31:24 +0530

    Create 1440-ConvertIntegerToTheSumOfTwoNoZeroIntegers

---
+++ 1440-ConvertIntegerToTheSumOfTwoNoZeroIntegers/1440-ConvertIntegerToTheSumOfTwoNoZeroIntegers.py
@@ -0,0 +1,8 @@
+# Last updated: 7/18/2026, 8:31:24 PM
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        for A in range(1, n):
+            B = n - A
+            if "0" not in str(A) + str(B):
+                return [A, B]
+        return []
