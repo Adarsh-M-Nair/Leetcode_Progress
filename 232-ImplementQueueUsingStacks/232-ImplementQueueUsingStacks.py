fe5073bc7c1a526c3f11397739096c5aaed08f8c
Adarsh M. Nair <106982376+Adarsh-M-Nair@users.noreply.github.com>
2026-07-18 20:32:05 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.py
@@ -0,0 +1,30 @@
+# Last updated: 7/18/2026, 8:32:05 PM
+class MyQueue:
+
+    def __init__(self):
+        self.queue=[]
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        return None
+
+    def pop(self) -> int:
+        return self.queue.pop(0)
+
+    def peek(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        if len(self.queue)==0:
+            return True
+        else:
+            return False
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
